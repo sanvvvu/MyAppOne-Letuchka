ee62e57e9a0d1cffffd216a3e812d70f7572b2ca
sanvvu <kekovaekaterina@yandex.ru>
2025-08-13 15:44:37 +0400

    упрощенная версия

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,24 +1,14 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using Avalonia.Controls.ApplicationLifetimes; // Добавлено
-using Avalonia.Diagnostics; // Добавлено для AttachDevTools
 
 namespace MyTestOne.Views;
 
-public partial class MainWindow : Window
+public class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
-        
-#if DEBUG
-    // Упрощенная версия без DevTools
-    if (Design.IsDesignMode)
-    {
-        this.InitializeComponent();
-    }
-#endif
     }
 
     private void InitializeComponent()
