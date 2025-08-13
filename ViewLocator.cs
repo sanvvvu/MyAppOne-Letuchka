f57d96f985cac0ffd4007d563ad650eca195f61f
sanvvu <kekovaekaterina@yandex.ru>
2025-08-13 15:50:40 +0400

    оптимизация

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -1,31 +1,23 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using MyTestOne.ViewModels;
 
 namespace MyTestOne;
 
 public class ViewLocator : IDataTemplate
 {
-
     public Control? Build(object? param)
     {
-        if (param is null)
-            return null;
+        if (param is null) return null;
         
-        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var name = param.GetType().FullName!
+            .Replace("ViewModels", "Views")
+            .Replace("ViewModel", "View");
+            
         var type = Type.GetType(name);
-
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-        
-        return new TextBlock { Text = "Not Found: " + name };
+        return type != null ? (Control)Activator.CreateInstance(type)! : 
+            new TextBlock { Text = "Not Found: " + name };
     }
 
-    public bool Match(object? data)
-    {
-        return data is ViewModelBase;
-    }
+    public bool Match(object? data) => data is not null;
 }
