9221c317b90e6dc9d696605308cf297f61cfa6ea
sanvvu <kekovaekaterina@yandex.ru>
2025-08-13 15:43:20 +0400

    Update ViewModelBase.cs
    Добавила using System.Collections.Generic; выдавало из-за нее ошибку

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,26 +1,25 @@
-using System.Collections.Generic;
+using System;
+using System.Collections.Generic; // Добавлено
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using ReactiveUI;
 
-namespace MyTestOne.ViewModels;
-
-public class ViewModelBase : ReactiveObject
+namespace MyTestOne.ViewModels
 {
-    // Явно инициализируем событие как null
-    public new event PropertyChangedEventHandler? PropertyChanged = null;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    public class ViewModelBase : INotifyPropertyChanged
     {
-        // Добавляем проверку на null перед вызовом
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
