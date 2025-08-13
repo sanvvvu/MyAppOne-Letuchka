bff79eb896c3640b2546918f72eacc9704b82c34
sanvvu <kekovaekaterina@yandex.ru>
2025-08-13 15:50:02 +0400

    добавление логов на случай ошибок

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
-﻿using Avalonia;
-using System; // Добавлено для STAThread
-using Avalonia.Controls;
+using System;
+using Avalonia;
+using System.IO;
 
 namespace MyTestOne;
 
 public class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // Создаем лог-файл
+            File.WriteAllText("startup.log", "Запуск приложения...");
+            
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            // Записываем критическую ошибку
+            File.WriteAllText("critical_error.log", $"КРИТИЧЕСКАЯ ОШИБКА:\n{ex}");
+        }
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .LogToTrace();
+            .WithInterFont()
+            .LogToTrace(); // Включаем подробное логирование
 }
